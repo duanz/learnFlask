d55a8cdb3242abdd3f041b5c9b68670a7498adae
duanz <918080313@qq.com>
2017-07-11 12:17:12 +0800

    the first commit of this project

---
+++ config.py
@@ -0,0 +1,14 @@
+# -*-coding:utf-8 -*-
+
+
+class Config:
+    # MYSQL_DATABASE_URI = {'host': 'localhost', 'user': 'root', 'password': '', 'database': 'duan',
+    #                       'port': 3306, 'charset': 'utf8'}
+    # MYSQL_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/duan?charset=utf8"
+    pass
+
+
+config = {
+    'MYSQL_DATABASE_URI': "localhost, root, , duan, 3306, utf8"
+}
+
